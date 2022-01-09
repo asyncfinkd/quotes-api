accf1cc7340f6a38df3ad9bebfcc1aa01b38877d
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-10 01:54:49 +0400

    add Authorization by JWT authentication

--- handler/auth_handler.go
+++ handler/auth_handler.go
@@ -1,6 +1,12 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+)
 
 type User struct {
 	ID       uint   `json:"id"`
@@ -11,9 +17,51 @@ type User struct {
 }
 
 var users = []*User{
-	{ID: 0, Name: "Nika Shamiladze", Email: "zxc@gmail.com", Password: "123", Role: "user"},
+	{ID: 0, Name: "Nika Shamiladze", Email: "zxc@gmail.com", Password: "123123123", Role: "user"},
 }
 
 func Signin(ctx *fiber.Ctx) error {
-	return ctx.Status(fiber.StatusOK).JSON("...")
+	type Request struct {
+		Email    *string `json:"email"`
+		Password *string `json:"password"`
+	}
+
+	var body Request
+
+	if err := ctx.BodyParser(&body); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse body",
+		})
+	}
+
+	if !strings.Contains(*body.Email, "@") {
+		return ctx.Status(fiber.StatusBadRequest).JSON("email address you entered doesn't contain '@' sign")
+	}
+
+	if len(*body.Password) < 6 {
+		return ctx.Status(fiber.StatusBadRequest).JSON("password you entered is too short")
+	}
+
+	for _, t := range users {
+		loginValidate := t.Email == *body.Email && t.Password == *body.Password
+		if loginValidate {
+			token := jwt.New(jwt.SigningMethodHS256)
+			claims := token.Claims.(jwt.MapClaims)
+			claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+			t, err := token.SignedString([]byte("secret"))
+
+			if err != nil {
+				return ctx.SendStatus(fiber.StatusInternalServerError)
+			}
+
+			return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+				"success":      true,
+				"message":      "Congratulation, you logged succesfully",
+				"access_token": t,
+			})
+		}
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON("something is wrong")
 }
