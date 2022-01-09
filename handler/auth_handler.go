b0f31c73edb4bcc7fd823cf1ff10ade2b40d8662
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-10 02:17:32 +0400

    Add Bcrypt Validator, Environment Keyword

--- handler/auth_handler.go
+++ handler/auth_handler.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"quotes-api/config"
 	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -17,7 +19,12 @@ type User struct {
 }
 
 var users = []*User{
-	{ID: 0, Name: "Nika Shamiladze", Email: "zxc@gmail.com", Password: "123123123", Role: "user"},
+	{ID: 0, Name: "Nika Shamiladze", Email: "zxc@gmail.com", Password: "$2a$12$0IelvstJ1QLvFZOH8GM8dOuzu/ouhBNE2DJ3GpfK79dzZ4mO5JuHu", Role: "user"},
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
 
 func Signin(ctx *fiber.Ctx) error {
@@ -35,21 +42,25 @@ func Signin(ctx *fiber.Ctx) error {
 	}
 
 	if !strings.Contains(*body.Email, "@") {
-		return ctx.Status(fiber.StatusBadRequest).JSON("email address you entered doesn't contain '@' sign")
+		return ctx.Status(fiber.StatusUnauthorized).JSON("email address you entered doesn't contain '@' sign")
 	}
 
 	if len(*body.Password) < 6 {
-		return ctx.Status(fiber.StatusBadRequest).JSON("password you entered is too short")
+		return ctx.Status(fiber.StatusUnauthorized).JSON("password you entered is too short")
 	}
 
 	for _, t := range users {
-		loginValidate := t.Email == *body.Email && t.Password == *body.Password
+		if !CheckPasswordHash(*body.Password, t.Password) {
+			return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"success": false, "message": "Invalid password", "access_token": nil})
+		}
+
+		loginValidate := t.Email == *body.Email
 		if loginValidate {
 			token := jwt.New(jwt.SigningMethodHS256)
 			claims := token.Claims.(jwt.MapClaims)
 			claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
-			t, err := token.SignedString([]byte("secret"))
+			t, err := token.SignedString([]byte(config.Config("SECRET_KEY")))
 
 			if err != nil {
 				return ctx.SendStatus(fiber.StatusInternalServerError)
