303758050c9a1016f019b5b12142cf2427bab6a7
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-11 02:17:26 +0400

    validate all struct

--- constant/app_constant.go
+++ constant/app_constant.go
@@ -1,23 +1,23 @@
 package constant
 
 type Quotes struct {
-	ID       uint     `json:"id"`
-	Text     string   `json:"text"`
-	Author   string   `json:"author"`
-	Category []string `json:"category"`
+	ID       uint     `json:"id" validate:"required,omitempty"`
+	Text     string   `json:"text" validate:"required"`
+	Author   string   `json:"author" validate:"required"`
+	Category []string `json:"category" validate:"category"`
 }
 
 type AuthorGallery struct {
-	ID       uint     `json:"id"`
-	Url      string   `json:"url"`
-	Category []string `json:"category"`
-	Name     string   `json:"name"`
+	ID       uint     `json:"id" validate:"required,omitempty"`
+	Url      string   `json:"url" validate:"required"`
+	Category []string `json:"category" validate:"required"`
+	Name     string   `json:"name" validate:"required"`
 }
 
 type User struct {
-	ID       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
+	ID       uint   `json:"id" validate:"required,omitempty"`
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required,gte=10"`
+	Role     string `json:"role" validate:"required"`
 }
