9ff68374d221e10380193c25190f7fa67c280749
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-12 02:31:12 +0400

    Get All Items

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -47,10 +47,16 @@ var authorGallery = []*constant.AuthorGallery{
 // @Success 200 {array} Quotes{}
 // @Router /api/quotes [get]
 func GetQuotes(ctx *fiber.Ctx) error {
-	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
-		"success": true,
-		"item":    quotes,
-	})
+	point := ctx.Params("point")
+
+	if point == "all" {
+		return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"item":    quotes,
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON("...")
 }
 
 // @Summary Get once Quotes
