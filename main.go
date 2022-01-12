49f822260956112d75a7fa0d3282608f61f08473
asyncfinkd <shamgoingzuck@gmail.com>
2022-01-12 15:53:33 +0400

    Delete Comments

--- main.go
+++ main.go
@@ -20,33 +20,10 @@ import (
 func main() {
 	// Connect with database
 	database.ConnectDb()
-	// if err := Connect(); err != nil {
-	// log.Fatal(err)
-	// }
 
 	app := fiber.New()
 
 	app.Use(cors.New())
-
-	// app.Get("/test/db", func(ctx *fiber.Ctx) error {
-	// 	rows, err := db.Query("SELECT * FROM quotes")
-	// 	if err != nil {
-	// 		return ctx.Status(500).SendString(err.Error())
-	// 	}
-	// 	defer rows.Close()
-	// 	result := Todos{}
-
-	// 	for rows.Next() {
-	// 		employee := Todo{}
-	// 		if err := rows.Scan(&employee.Id, &employee.Text, &employee.Author); err != nil {
-	// 			return err // Exit if we get an error
-	// 		}
-
-	// 		result.Todos = append(result.Todos, employee)
-	// 	}
-
-	// 	return ctx.Status(fiber.StatusOK).JSON(result)
-	// })
 
 	router.SetupUserRoutes(app)
 	router.SetupAuthRoutes(app)
